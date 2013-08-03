0635609341992182504b21edba6fa1b8a53e7cfc
kc <choudhury.khanin@gmail.com>
2013-08-04 00:04:38 +0530

    add x-code  test project

---
+++ testProgram/testProgram/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  testProgram
+//
+//  Created by Khanin Choudhury on 03/08/13.
+//  Copyright (c) 2013 furo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ testProgram/testProgram/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  testProgram
+//
+//  Created by Khanin Choudhury on 03/08/13.
+//  Copyright (c) 2013 furo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
